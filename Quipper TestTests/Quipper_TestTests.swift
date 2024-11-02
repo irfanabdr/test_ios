256ef02105aaf7fb9beb3ce03f890c3461e4800e
Irfan <irfan.abdrasyid@gmail.com>
2024-11-02 08:34:20 +0700

    Initial Commit

---
+++ Quipper TestTests/Quipper_TestTests.swift
@@ -0,0 +1,17 @@
+//
+//  Quipper_TestTests.swift
+//  Quipper TestTests
+//
+//  Created by Irfan on 02/11/24.
+//
+
+import Testing
+@testable import Quipper_Test
+
+struct Quipper_TestTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
